dad6b696e5ef062cb9943f37e548b11fa10cd2cf
namunaru <frontierjoe50@gmail.com>
2025-12-28 20:47:01 +0900

    Update client.ts

--- src/lib/notion/client.ts
+++ src/lib/notion/client.ts
@@ -1,1 +1,8 @@
+import { Client } from '@notionhq/client';
+
+export const notion = new Client({
+  auth: import.meta.env.NOTION_TOKEN,
+});
+
+export const DATABASE_ID = import.meta.env.NOTION_DATABASE_ID;
 
